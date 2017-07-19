a8acd4ae62e3b9f1e442d16d9c25979aeae3e182
danielstaleiny <daniel.rafaj@gmail.com>
2017-07-19 15:53:49 +0200

    I found out that I am utilizing at most 128M of memory. so I down it down. at my largest scale 5000x5000 it timedout before returning picture so I rasied timeout to 15 seconds of execution.

--- lambda/index.js
+++ lambda/index.js
@@ -14,53 +14,89 @@ exports.handler = function(event, context, callback) {
   if(key === undefined){
     return callback(null, {
         statusCode: '400',
-        body: {msg: 'Key does not exists.'}
+        body: JSON.stringify({ error: 'Key does not exists.' }),
+        headers: {
+        'Content-Type': 'application/json'
+        }
       })
   }
   const match = key.match(/(\d+)x?(\d+)?\/(.+\.(png|PNG|jpg|JPG|jpeg|JPEG|tif|TIF|tiff|TIFF|webp|WEBP))/);
   if(match === null){
-    return callback(null, {
-        statusCode: '400',
-        body: {msg: 'Key does not match form: Nx?N?/name.[jpg|png|tiff|webp]. Not supported image format.'}
-      })
-  }
-  let height = 0;
-  const width = parseInt(match[1], 10);
-  if(match[2] === undefined){
-    height = parseInt(match[1], 10);
-  }else {
-    height = parseInt(match[2], 10);
-  }
-  const maxPixelCount = 2500;
-  const minPixelCount = 0;
-  if(width <= minPixelCount || width > maxPixelCount || height <= minPixelCount || height > maxPixelCount) {
-    return callback(null, {
-        statusCode: '400',
-        body: {msg: 'Image requested is not in range ${minPixelCount+=1}-${maxPixelCount} pixels.'}
-      })
-  }
-  const originalKey = match[3];
+    S3.headObject({Bucket: BUCKET, Key: key}, function(err, data) {
+      if(err){
+        return callback(null, {
+            statusCode: '404',
+            body: JSON.stringify({ error: 'Key does not exists.' }),
+            headers: {
+            'Content-Type': 'application/json'
+            }
+          })
+      }else {
+        return callback(null, {
+            statusCode: '400',
+            body: JSON.stringify({ error: 'Key does not match form: Nx?N?/name.[jpeg|jpg|png|tiff|webp]. Not supported image format.' }),
+            headers: {
+            'Content-Type': 'application/json'
+            }
+          })
+      }
+    })
 
-  S3.getObject({Bucket: BUCKET, Key: originalKey}).promise()
-    .then(data => Sharp(data.Body)
-      .resize(width, height)
-      .max()
-      .toFormat('jpeg')
-      .toBuffer()
-    )
-    .then(buffer => S3.putObject({
-        Body: buffer,
-        Bucket: BUCKET,
-        ContentType: 'image/jpeg',
-        Key: key,
-        Tagging: "resized=true"
-      }).promise()
-    )
-    .then(() => callback(null, {
+  }else {
+    const originalKey = match[3];
+    S3.headObject({Bucket: BUCKET, Key: originalKey}, function(err, data) {
+      if(err){
+        return callback(null, {
+            statusCode: '404',
+            body: JSON.stringify({ error: 'Key does not exists.' }),
+            headers: {
+            'Content-Type': 'application/json'
+            }
+          })
+        }else{
+          let height = 0;
+          const width = parseInt(match[1], 10);
+          if(match[2] === undefined){
+            height = parseInt(match[1], 10);
+          }else {
+            height = parseInt(match[2], 10);
+          }
+          const maxPixelCount = 5000;
+          const minPixelCount = 0;
+          if(width <= minPixelCount || width > maxPixelCount || height <= minPixelCount || height > maxPixelCount) {
+            return callback(null, {
+              statusCode: '400',
+              body: JSON.stringify({ error: `Image requested is not in range ${minPixelCount+=1}-${maxPixelCount} pixels.` }),
+              headers: {
+                'Content-Type': 'application/json'
+              }
+            })
+          }
+
+          S3.getObject({Bucket: BUCKET, Key: originalKey}).promise()
+          .then(data => Sharp(data.Body)
+          .resize(width, height)
+          .max()
+          .toFormat('jpeg')
+          .toBuffer()
+        )
+        .then(buffer => S3.putObject({
+          Body: buffer,
+          Bucket: BUCKET,
+          ContentType: 'image/jpeg',
+          Key: key,
+          Tagging: "resized=true"
+        }).promise()
+      )
+      .then(() => callback(null, {
         statusCode: '301',
         headers: {'location': `${URL}/${key}`},
         body: ''
       })
     )
     .catch(err => callback(err))
+        }
+    })
+
+  }
 }
