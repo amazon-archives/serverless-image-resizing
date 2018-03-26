23c42dcc30bbdd2524b1124110076d38680fb5de
Shim Won <marocchino@gmail.com>
2018-03-26 18:26:55 +0900

    feat: trannsform to webp
    | url                  | input    | output               |
    | -------------------- | -------- | -------------------- |
    | 100x100/aaa.png.webp | /aaa.png | 100x100/aaa.png.webp |
    | 100x100/aaa.png      | /aaa.png | 100x100/aaa.png      |

--- lambda/index.js
+++ lambda/index.js
@@ -17,11 +17,21 @@ if (process.env.ALLOWED_DIMENSIONS) {
 
 exports.handler = function(event, context, callback) {
   const key = event.queryStringParameters.key;
-  const match = key.match(/((\d+)x(\d+))\/(.*)/);
+  const match = key.match(/^((\d+)x(\d+))\/(.*?)(\.webp)?(\?.*)?$/);
   const dimensions = match[1];
   const width = parseInt(match[2], 10);
   const height = parseInt(match[3], 10);
-  const originalKey = match[4];
+  const originalKey = (match[4] || '') + (match[6] || '');
+  const isWebp = match[5];
+
+  if(originalKey.match(/^\d+x\d+\/.*/)) {
+     callback(null, {
+      statusCode: '403',
+      headers: {},
+      body: '',
+    });
+    return;
+  }
 
   if(ALLOWED_DIMENSIONS.size > 0 && !ALLOWED_DIMENSIONS.has(dimensions)) {
      callback(null, {
@@ -35,13 +45,13 @@ exports.handler = function(event, context, callback) {
   S3.getObject({Bucket: BUCKET, Key: originalKey}).promise()
     .then(data => Sharp(data.Body)
       .resize(width, height)
-      .toFormat('png')
+      .toFormat(isWebp ? 'webp' : 'png')
       .toBuffer()
     )
     .then(buffer => S3.putObject({
         Body: buffer,
         Bucket: BUCKET,
-        ContentType: 'image/png',
+        ContentType: isWebp ? 'image/webp' : 'image/png',
         Key: key,
       }).promise()
     )
