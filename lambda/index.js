de197181d3b435e3c29b6fe6dd22d2a5c2b462a3
Dongwoo Gim <gimdongwoo@gmail.com>
2018-05-29 01:19:42 +0900

    use max(), add rotate()

--- lambda/index.js
+++ lambda/index.js
@@ -15,7 +15,7 @@ if (process.env.ALLOWED_DIMENSIONS) {
   dimensions.forEach((dimension) => ALLOWED_DIMENSIONS.add(dimension));
 }
 
-exports.handler = function (event, context, callback) {
+exports.handler = function(event, context, callback) {
   const key = event.queryStringParameters.key;
   const match = key.match(/((\d*)x(\d*))\/(.*)/);
   const dimensions = match[1];
@@ -25,8 +25,8 @@ exports.handler = function (event, context, callback) {
   };
   const originalKey = match[4];
 
-  if (ALLOWED_DIMENSIONS.size > 0 && !ALLOWED_DIMENSIONS.has(dimensions)) {
-    callback(null, {
+  if(ALLOWED_DIMENSIONS.size > 0 && !ALLOWED_DIMENSIONS.has(dimensions)) {
+     callback(null, {
       statusCode: '403',
       headers: {},
       body: '',
@@ -34,65 +34,26 @@ exports.handler = function (event, context, callback) {
     return;
   }
 
-  const imageResize = (img) => {
-    img.resize(size.width, size.height)
+  S3.getObject({Bucket: BUCKET, Key: originalKey}).promise()
+    .then(data => Sharp(data.Body)
+      .resize(size.width || size.height, size.height || size.width)
+      .max()
+      .rotate()
       .toFormat('png')
       .toBuffer()
-      .then(buffer => S3.putObject({
+    )
+    .then(buffer => S3.putObject({
         Body: buffer,
         Bucket: BUCKET,
         ContentType: 'image/png',
         Key: key,
-      }).promise())
-      .then(() => callback(null, {
+      }).promise()
+    )
+    .then(() => callback(null, {
         statusCode: '301',
-        headers: {
-          'location': `${URL}/${key}`
-        },
+        headers: {'location': `${URL}/${key}`},
         body: '',
-      }))
-      .catch(err => callback(err))
-  };
-
-  S3.getObject({
-      Bucket: BUCKET,
-      Key: originalKey
-    }).promise()
-    .then(data => {
-      const img = Sharp(data.Body);
-      if (size.width && size.height) {
-        return imageResize(img);
-      }
-      // resized long
-      return img
-        .metadata()
-        .then((metadata) => {
-          const resizedLong = Math.round(size.width || size.height || 900);
-          const {
-            width,
-            height
-          } = metadata;
-
-          size.width = width;
-          size.height = height;
-          // resize
-          if (resizedLong < width || resizedLong < height) {
-            if (width < height) {
-              // landsacpe : 2000 * 3000
-              size.width = width / height * resizedLong; // 0.66 * 300 = 200
-              size.height = resizedLong; // 300
-            } else {
-              // portrait : 3000 * 2000
-              size.width = resizedLong; // 300
-              size.height = height / width * resizedLong; // 1.5 * 300 = 200
-            }
-
-            // round
-            size.width = Math.round(size.width);
-            size.height = Math.round(size.height);
-          }
-
-          return imageResize(img);
-        });
-    })
+      })
+    )
+    .catch(err => callback(err))
 }
